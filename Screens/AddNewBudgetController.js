d34acf3a08ea86b2a618c2eea9767609586efba8
PuBHARGAVI <saibhargavi.puvvada@gmail.com>
2023-05-24 01:15:42 +0530

    refactor: append prefix ADD to start & end date in controller to improve the readability

--- Screens/AddNewBudgetController.js
+++ Screens/AddNewBudgetController.js
@@ -11,8 +11,8 @@ export function useAddBudgetScreen() {
 
   return {
     ADD_AMOUNT: amount => service.send(events.ADD_AMOUNT(amount)),
-    START_DATE: startDate => service.send(events.START_DATE(startDate)),
-    END_DATE: endDate => service.send(events.END_DATE(endDate)),
+    ADD_START_DATE: startDate => service.send(events.START_DATE(startDate)),
+    ADD_END_DATE: endDate => service.send(events.END_DATE(endDate)),
     showStartDatePicker: useSelector(service, selectStartDatePickerVisibility),
     showEndDatePicker: useSelector(service, selectEndDatePickerVisibility),
   };
