37c829e6c11dcdc546999185710592631419d046
PuBHARGAVI <saibhargavi.puvvada@gmail.com>
2023-05-26 20:31:18 +0530

    feat: add styles to add budget screen text input and label

--- Screens/AddNewBudget.js
+++ Screens/AddNewBudget.js
@@ -48,64 +48,71 @@ export const AddNewBudget = () => {
       start={{x: 0, y: 0}}
       end={{x: 1, y: 1}}
       style={styles.container}>
-      <View style={styles.viewContainer}>
-        <Text style={styles.label}>Budget:</Text>
-        <TextInput
-          style={styles.input}
-          keyboardType="numeric"
-          placeholder="Enter the Amount"
-          placeholderTextColor="#696969"
-          value={budgetAmount}
-          autoFocus={true}
-          onChangeText={text => handleBudgetInput(text)}
-        />
-      </View>
-      <View style={styles.viewContainer}>
-        <Text style={styles.label}>Start Date:</Text>
-        <Pressable
-          onPress={() => setStartDatePickerVisibility(!showStartDatePicker)}>
+      <View>
+        <View style={styles.viewContainer}>
+          <Text style={styles.label}>Budget</Text>
+          <Text style={styles.colonStyles}>:</Text>
           <TextInput
-            style={styles.input}
-            placeholder="Select the Date"
-            value={startDate}
+            style={[styles.input, {flex: 0.6}]}
+            keyboardType="numeric"
+            placeholder="Enter the Amount"
+            placeholderTextColor="#696969"
+            value={budgetAmount}
             autoFocus={true}
-            editable={false}
+            onChangeText={text => handleBudgetInput(text)}
           />
-        </Pressable>
-      </View>
-      <View style={styles.viewContainer}>
-        <Text style={styles.label}>End Date:</Text>
-        <Pressable
-          onPress={() => setEndDatePickerVisibility(!showEndDatePicker)}>
-          <TextInput
-            style={styles.input}
-            placeholder="Select the Date"
-            value={endDate}
-            autoFocus={true}
-            editable={false}
+        </View>
+        <View style={styles.viewContainer}>
+          <Text style={styles.label}>Start Date</Text>
+          <Text style={styles.colonStyles}>:</Text>
+          <Pressable
+            style={styles.pressableStyles}
+            onPress={() => setStartDatePickerVisibility(!showStartDatePicker)}>
+            <TextInput
+              style={styles.input}
+              placeholder="Select the Date"
+              value={startDate}
+              autoFocus={true}
+              editable={false}
+            />
+          </Pressable>
+        </View>
+        <View style={styles.viewContainer}>
+          <Text style={styles.label}>End Date</Text>
+          <Text style={styles.colonStyles}>:</Text>
+          <Pressable
+            style={styles.pressableStyles}
+            onPress={() => setEndDatePickerVisibility(!showEndDatePicker)}>
+            <TextInput
+              style={styles.input}
+              placeholder="Select the Date"
+              value={endDate}
+              autoFocus={true}
+              editable={false}
+            />
+          </Pressable>
+        </View>
+        {showStartDatePicker && (
+          <DateTimePicker
+            mode="date"
+            display="spinner"
+            value={new Date()}
+            positiveButton={{label: 'OK', textColor: '#1e90ff'}}
+            negativeButton={{label: 'CANCEL', textColor: '#1e90ff'}}
+            onChange={handleStartDateOnChange}
           />
-        </Pressable>
+        )}
+        {showEndDatePicker && (
+          <DateTimePicker
+            mode="date"
+            display="spinner"
+            value={new Date()}
+            positiveButton={{label: 'OK', textColor: '#1e90ff'}}
+            negativeButton={{label: 'CANCEL', textColor: '#1e90ff'}}
+            onChange={handleEndDateOnChange}
+          />
+        )}
       </View>
-      {showStartDatePicker && (
-        <DateTimePicker
-          mode="date"
-          display="spinner"
-          value={new Date()}
-          positiveButton={{label: 'OK', textColor: '#1e90ff'}}
-          negativeButton={{label: 'CANCEL', textColor: '#1e90ff'}}
-          onChange={handleStartDateOnChange}
-        />
-      )}
-      {showEndDatePicker && (
-        <DateTimePicker
-          mode="date"
-          display="spinner"
-          value={new Date()}
-          positiveButton={{label: 'OK', textColor: '#1e90ff'}}
-          negativeButton={{label: 'CANCEL', textColor: '#1e90ff'}}
-          onChange={handleEndDateOnChange}
-        />
-      )}
       <Button title="Add the budget" />
     </LinearGradient>
   );
@@ -115,23 +122,33 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
   },
   input: {
     borderBottomColor: '#1e90ff',
     borderBottomWidth: 1,
     color: '#696969',
   },
+  colonStyles: {
+    fontWeight: '500',
+    fontSize: 17,
+    color: 'black',
+    flex: 0.1,
+  },
+  pressableStyles: {
+    flex: 0.6,
+  },
   viewContainer: {
     flexDirection: 'row',
     alignItems: 'center',
     marginBottom: 15,
     justifyContent: 'space-evenly',
+    width: '80%',
   },
   label: {
     color: 'black',
     fontWeight: '500',
     fontSize: 17,
-    width: 110,
+    flex: 0.3,
   },
 });
