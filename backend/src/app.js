27c36fce835a910523c131b3a3b87c39c81990a2
PuBHARGAVI <46226958+PuBHARGAVI@users.noreply.github.com>
2023-10-02 15:32:14 +0530

    refactor: use mongoose package to store and create model for user

---
+++ backend/src/app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/expenses-tracker',{
+    useNewUrlParser: true
+}).then((res) => {
+  console.log('Connected to MongoDB',res);
+})
